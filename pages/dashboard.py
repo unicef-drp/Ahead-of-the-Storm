fa5d6cada59e337a81cbeb9577c2dc3de725bbcf
felixschwebel <schwebel.felix@gmail.com>
2025-10-29 17:46:12 +0100

    updated action file

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -51,10 +51,10 @@ from gigaspatial.processing.geo import convert_to_geodataframe
 from components.data.data_store_utils import get_data_store
 
 ##### env variables #####
-RESULTS_DIR = config.RESULTS_DIR
-BBOX_FILE = config.BBOX_FILE
-VIEWS_DIR = config.VIEWS_DIR
-ROOT_DATA_DIR = config.ROOT_DATA_DIR
+RESULTS_DIR = config.RESULTS_DIR or "project_results/climate/lacro_project"
+BBOX_FILE = config.BBOX_FILE or "bbox.parquet"
+VIEWS_DIR = config.VIEWS_DIR or "aos_views"
+ROOT_DATA_DIR = config.ROOT_DATA_DIR or "geodb"
 #########################
 
 # Initialize data store using centralized utility
